29ddeaf45070e3ba3939ed83b3f4b58e1aaaabf0
FeeDeee! <fedezuriaga@gmail.com>
2025-01-26 20:35:28 -0300

    fix: creating userCourse

---
+++ backend/src/userCourse/userCourse.controller.ts
@@ -0,0 +1,54 @@
+import {
+    Body,
+    ClassSerializerInterceptor,
+    Controller,
+    Delete,
+    HttpCode,
+    HttpStatus,
+    Post,
+    Get,
+    Param,
+    UseGuards,
+    UseInterceptors,
+    NotFoundException,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+
+import { JwtGuard } from '../auth/guards/jwt.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { EnrollUserCourseDto, UnenrollUserCourseDto } from './userCourse.dto';
+import { UserCourse } from './userCourse.entity';
+import { UserCourseService } from './userCourse.service';
+
+@Controller('usercourses')
+@ApiTags('UserCourses')
+@ApiBearerAuth()
+@UseGuards(JwtGuard, RolesGuard)
+@UseInterceptors(ClassSerializerInterceptor)
+export class UserCourseController {
+    constructor(private readonly userCourseService: UserCourseService) { }
+
+    @Get()
+    async getUserCourses(@Param('userId') userId: string) {
+        const response = await this.userCourseService.findAllUserCourses(userId);
+        console.log("=".repeat(50))
+        console.log(response)
+        console.log("=".repeat(50))
+
+        if (!response || response.length === 0) {
+            throw new NotFoundException('No courses found for this user');
+        }
+        return response
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async save(@Body() enrollUserCourseDto: EnrollUserCourseDto): Promise<UserCourse> {
+        return await this.userCourseService.save(enrollUserCourseDto);
+    }
+
+    @Delete()
+    async delete(@Body() unenrollUserCourseDto: UnenrollUserCourseDto): Promise<string> {
+        return await this.userCourseService.delete(unenrollUserCourseDto);
+    }
+}
---
+++ backend/src/userCourse/userCourse.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Unique, Column, BaseEntity, JoinColumn } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Course } from '../course/course.entity';
+
+@Entity()
+@Unique(['user', 'course'])
+export class UserCourse extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: string;
+
+  @Column()
+  courseId: string;
+
+  @ManyToOne(() => User, (user) => user.courses, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @ManyToOne(() => Course, (course) => course.users, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'courseId' })
+  course: Course;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', nullable: true })
+  dateJoined: Date;
+}
